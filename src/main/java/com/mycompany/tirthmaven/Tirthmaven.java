e3e63b1f54a9a5605a4c0a287282d4527adb4caf
unknown <NMCC STUDENT@MUM15221CPU1425.SVKMGRP.COM>
2025-12-04 08:23:12 +0530

    First Commit

---
+++ src/main/java/com/mycompany/tirthmaven/Tirthmaven.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tirthmaven;
+
+/**
+ *
+ * @author NMCC STUDENT
+ */
+public class Tirthmaven {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
